f0724e11f6cd7ce5df8e9f629f0f6ea27cef22b3
kayshaou <kaysha.ou@gmail.com>
2023-01-22 12:30:10 +0800

    add rest calls

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
+import axios, {isCancel, AxiosError} from 'axios';
 
 function Order(props) {
   const [orderStatus, setOrderStatus] = useState("New");
+  const [id, setId] = useState('');
+  const [htmlUrl, setHtmlUrl] = useState('');
+
+  async function placeOrder(event) {
+        event.preventDefault();
+        const url = process.env.REACT_APP_API_URL+'kayshaou';
+        console.log(url)
+        const res = await axios.get(url);
+
+        setHtmlUrl(res.data.html_url)
+        setId(res.data.id);
+        if(res.data.id!=undefined) {
+        console.log("id ",res.data.id)
+            setOrderStatus('Initialized');
+        }
+  }
+
 
   return (
     <div class="my-7 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <div class="text-center space-y-2 sm:text-left">
         <div class="space-y-0.5">
           <p class="text-lg text-black font-semibold">Order</p>
-          <p class="text-slate-500 font-medium">{orderStatus}</p>
+           <p class="text-slate-500 font-medium">{id}</p>
+           <p class="text-slate-500 font-medium">{htmlUrl}</p>
         </div>
-        <button class="px-4 py-1 text-sm text-green-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-green-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Place
+        <button class="px-4 py-1 text-sm text-green-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-green-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+         onClick={(e) => placeOrder(e)}>
+          {id ? 'Initialized':'Place'}
         </button>
       </div>
     </div>
